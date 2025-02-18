96016811fad2db51c9584c4c78f3a747735a8bb2
Alexander Denev <aleksandar.d.denev@gmail.com>
2025-02-18 23:34:05 +0200

    OBLX-7 Updated fonts. Styled index page.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,29 +5,31 @@ const userStore = useUserStore()
 </script>
 
 <template>
-  <q-page class="row">
-    <div class="col-6 q-pa-md">
-      <q-card class="bg-positive">
-        <q-card-section> Grade </q-card-section>
-        <q-card-section>
-          {{ userStore.account.grade }}
-        </q-card-section>
-      </q-card>
-
-      <q-card class="bg-info">
-        <q-card-section> Grade Count </q-card-section>
-        <q-card-section>
-          {{ userStore.account.gradeCount }}
-        </q-card-section>
-      </q-card>
-      <q-card class="bg-negative">
-
-        <q-card-section> Grade </q-card-section>
-        <q-card-section>
-          {{ userStore.account.grade }}
-        </q-card-section>
-      </q-card>
+  <q-page class="row q-pt-sm">
+    <div class="col-6">
+      <div class="row no-wrap q-gutter-md q-pa-md">
+        <q-card class="bg-positive col" dark>
+          <q-card-section class="text-h5"> Grade </q-card-section>
+          <q-card-section>
+            {{ userStore.account.grade }}
+          </q-card-section>
+        </q-card>
+  
+        <q-card class="bg-info col" dark>
+          <q-card-section  class="text-h5"> Grade Count </q-card-section>
+          <q-card-section>
+            {{ userStore.account.gradeCount }}
+          </q-card-section>
+        </q-card>
+        <q-card class="bg-negative col" dark>
+          <q-card-section  class="text-h5"> Abscences </q-card-section>
+          <q-card-section>
+            {{ userStore.account.abscenceCount }}
+          </q-card-section>
+        </q-card>
+      </div>
     </div>
+
     <div class="col-6">
       <!-- Content for the right half -->
     </div>
