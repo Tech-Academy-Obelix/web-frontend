d260337e1d600b798675375d9ad454c5fa0499da
Александър Денев <aleksandar.d.denev@gmail.com>
2025-02-19 11:43:29 +0200

    OBLX-7 improved styling

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,23 +9,24 @@ const userStore = useUserStore()
     <div class="col-6">
       <div class="row no-wrap q-gutter-md q-pa-md">
         <q-card class="bg-positive col" dark>
-          <q-card-section class="text-h5"> Grade </q-card-section>
-          <q-card-section>
+          <q-card-section class="text-h4">
             {{ userStore.account.grade }}
           </q-card-section>
+          <q-card-section> Grade </q-card-section>
         </q-card>
-  
+
         <q-card class="bg-info col" dark>
-          <q-card-section  class="text-h5"> Grade Count </q-card-section>
-          <q-card-section>
+          <q-card-section class="text-h4">
             {{ userStore.account.gradeCount }}
           </q-card-section>
+          <q-card-section> Grade Count </q-card-section>
         </q-card>
+
         <q-card class="bg-negative col" dark>
-          <q-card-section  class="text-h5"> Abscences </q-card-section>
-          <q-card-section>
+          <q-card-section class="text-h4">
             {{ userStore.account.abscenceCount }}
           </q-card-section>
+          <q-card-section> Abscences </q-card-section>
         </q-card>
       </div>
     </div>
